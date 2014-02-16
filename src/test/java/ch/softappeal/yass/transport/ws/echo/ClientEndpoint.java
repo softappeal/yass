60517ebb54bbffea0aa93c12d4d8a1dcfe91fe0b
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2014-02-16 12:35:02 +0100

    websocket echo test added

---
+++ src/test/java/ch/softappeal/yass/transport/ws/echo/ClientEndpoint.java
@@ -0,0 +1,28 @@
+package ch.softappeal.yass.transport.ws.echo;
+
+import javax.websocket.CloseReason;
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.MessageHandler;
+import javax.websocket.Session;
+
+public final class ClientEndpoint extends Endpoint {
+
+  @Override public void onOpen(final Session session, final EndpointConfig config) {
+    System.out.println("session '" + session.getId() + "' opened");
+    session.addMessageHandler(new MessageHandler.Whole<String>() {
+      @Override public void onMessage(final String message) {
+        System.out.println("session '" + session.getId() + "' received message: '" + message + "'");
+      }
+    });
+  }
+
+  @Override public void onClose(final Session session, final CloseReason closeReason) {
+    System.out.println("session '" + session.getId() + "' close: " + closeReason);
+  }
+
+  @Override public void onError(final Session session, final Throwable thr) {
+    System.out.println("session '" + session.getId() + "' error: " + thr);
+  }
+
+}
---
+++ src/test/java/ch/softappeal/yass/transport/ws/echo/JettyClient.java
@@ -0,0 +1,35 @@
+package ch.softappeal.yass.transport.ws.echo;
+
+import org.eclipse.jetty.websocket.jsr356.ClientContainer;
+
+import javax.websocket.CloseReason;
+import javax.websocket.Session;
+import java.util.concurrent.TimeUnit;
+
+public final class JettyClient {
+
+  public static void main(final String... args) throws Exception {
+    final ClientContainer container = new ClientContainer();
+    container.start();
+    {
+      final Session session = container.connectToServer(new ClientEndpoint(), null, JettyServer.THE_URI);
+      session.getBasicRemote().sendText("hello from client 1");
+      TimeUnit.MILLISECONDS.sleep(100);
+      session.close();
+    }
+    {
+      final Session session = container.connectToServer(new ClientEndpoint(), null, JettyServer.THE_URI);
+      session.getBasicRemote().sendText("hello from client 2");
+      TimeUnit.MILLISECONDS.sleep(100);
+      session.close(new CloseReason(CloseReason.CloseCodes.PROTOCOL_ERROR, "My Reason"));
+    }
+    {
+      final Session session = container.connectToServer(new ClientEndpoint(), null, JettyServer.THE_URI);
+      System.out.println("isOpen: " + session.isOpen());
+      session.getBasicRemote().sendText("error");
+      TimeUnit.MILLISECONDS.sleep(100);
+      System.out.println("isOpen: " + session.isOpen());
+    }
+  }
+
+}
