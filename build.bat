dc0a01e1a1145fc04fba44674dde5ba4e2e94281
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2014-04-19 15:37:12 +0200

    updated to java 1.8.0_05

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.8.0
+set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.8.0_05
 set version=0.0
 set /p version=Version [ MAJOR.MINOR or 'enter' for %version% ]?:
 cmd /c C:\development\gradle-1.11\bin\gradle.bat -Pversion=%version%
