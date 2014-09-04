daf254677bd13557e5a4ff7e08904185d498f128
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2014-09-04 17:45:51 +0200

    util.Compiler moved to test

--- src/test/java/ch/softappeal/yass/util/test/CompilerTest.java
+++ src/test/java/ch/softappeal/yass/util/test/CompilerTest.java
@@ -4,31 +4,35 @@ import ch.softappeal.yass.util.Compiler;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CompilerTest {
 
-  public static final String TEXT = "Hello";
-
-  @Test public void test() throws Exception {
-    final String source = CompilerTestSource.class.getName();
-    final Map<String, CharSequence> map = new HashMap<>();
+  private static String add(final Map<String, CharSequence> map, final String name) throws IOException {
+    final String source = "ch.softappeal.yass.util.test." + name;
     map.put(
       source,
       Compiler.readFile(
-        new File("src/test/java", source.replace('.', '/') + Compiler.JAVA_EXT),
+        new File("src/test/java", source.replace('.', '/') + ".txt"),
         StandardCharsets.US_ASCII
       )
     );
+    return source;
+  }
+
+  @Test public void test() throws Exception {
+    final Map<String, CharSequence> map = new HashMap<>();
+    final String source = add(map, "CompilerTestSource");
+    add(map, "CompilerTestSourceText");
     final ClassLoader classLoader = Compiler.compile(
       CompilerTest.class.getClassLoader(),
       map,
       "-Xlint"
     );
     classLoader.loadClass(source).getMethod("x").invoke(null);
-    CompilerTestSource.x();
   }
 
 }
