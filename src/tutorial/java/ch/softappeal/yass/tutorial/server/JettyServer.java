ce67d3f218e6fac5f733873c4815b94953003323
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2014-11-09 11:18:13 +0100

    new fromatting

--- src/tutorial/java/ch/softappeal/yass/tutorial/server/JettyServer.java
+++ src/tutorial/java/ch/softappeal/yass/tutorial/server/JettyServer.java
@@ -9,18 +9,14 @@ public final class JettyServer extends WsServerSetup {
 
     public static void main(final String... args) throws Exception {
         final Server server = new Server();
-
         final ServerConnector serverConnector = new ServerConnector(server);
         serverConnector.setHost(HOST);
         serverConnector.setPort(PORT);
         server.addConnector(serverConnector);
-
         final ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
         contextHandler.setContextPath("/");
         server.setHandler(contextHandler);
-
         WebSocketServerContainerInitializer.configureContext(contextHandler).addEndpoint(ENDPOINT_CONFIG);
-
         server.start();
         System.out.println("started");
     }
