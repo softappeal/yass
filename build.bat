8595269e10bec56fab8a06144f0c83b7b02b9d93
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2015-02-21 19:42:48 +0100

    gradle updated

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_31
 set version=0.0.0
 set /p version=Version [ MAJOR.MINOR.PATCH or 'enter' for %version% ]?:
-cmd /c C:\development\gradle-2.3-rc-4\bin\gradle.bat -Pversion=%version%
+cmd /c C:\development\gradle-2.3\bin\gradle.bat -Pversion=%version%
 pause
