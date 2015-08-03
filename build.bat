0268387e7b0274259da5cf57c392eb8e8975b06b
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2015-08-03 18:31:22 +0200

    undertow and jetty updated jdk updated

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_45
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_51
 set version=0.0.0
 set /p version=Version [ MAJOR.MINOR.PATCH or 'enter' for %version% ]?:
 cmd /c C:\development\gradle-2.4\bin\gradle.bat -Pversion=%version%
