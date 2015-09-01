4130fbccefcbedd9f63097e4ec815392e03afcd3
AngeloSalvade <angelo.salvade@zkb.ch>
2015-09-01 12:59:33 +0200

    configuration

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
-set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_80
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_65
 set version=0.0.0
 set /p version=Version [ MAJOR.MINOR.PATCH or 'enter' for %version% ]?:
-cmd /c C:\development\gradle-2.3\bin\gradle.bat -Pversion=%version%
+cmd /c C:\Workarea\_tools\gradle-2.3\bin\gradle.bat -Pversion=%version%
 pause
