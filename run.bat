30b8e3c926f9d07538fd72d91d294db9a3635266
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2015-09-21 08:07:02 +0200

    cosmetic

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 cd build\server
-"C:\Program Files\Java\jdk1.8.0_60\bin\java.exe" -classpath build/classes/tutorial;build/classes/main;build/allJars/* ch.softappeal.yass.tutorial.server.web.JettyServer
+"C:\Program Files\Java\jdk1.8.0_60\bin\java.exe" -classpath libs/* ch.softappeal.yass.tutorial.server.web.JettyServer
