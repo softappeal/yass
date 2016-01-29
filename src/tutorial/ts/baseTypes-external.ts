92616622b0a224830e74b7087f71e5e4115d0a37
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2016-01-29 17:15:03 +0100

    tutorial.ts: baseTypes splitted into external/internal

---
+++ src/tutorial/ts/baseTypes-external.ts
@@ -0,0 +1,31 @@
+// shows how to use contract external base types
+
+/// <reference path="../../main/ts/yass"/>
+
+interface Integer {
+    get(): number;
+}
+
+class IntegerHandler implements yass.TypeHandler<Integer> {
+    read(reader: yass.Reader): Integer {
+        return new IntegerImpl(reader.readZigZagInt());
+    }
+    write(value: Integer, writer: yass.Writer): void {
+        writer.writeZigZagInt(value.get());
+    }
+    static TYPE_DESC = new yass.TypeDesc(yass.FIRST_ID, new IntegerHandler);
+}
+
+class IntegerImpl implements Integer {
+    constructor(private value: number) {
+        // note: check if value is really a Java Integer should be implemented here
+    }
+    get(): number {
+        return this.value;
+    }
+    toString(): string {
+        return "IntegerImpl(" + this.get() + ")";
+    }
+}
+
+(<any>IntegerImpl).TYPE_DESC = IntegerHandler.TYPE_DESC;
---
+++ src/tutorial/ts/baseTypes-internal.ts
@@ -0,0 +1,30 @@
+// shows how to use contract internal base types
+
+/// <reference path="../../main/ts/yass"/>
+
+namespace contract {
+
+    class ExpirationHandler implements yass.TypeHandler<Expiration> {
+        read(reader: yass.Reader): Expiration {
+            return new Expiration(
+                reader.readZigZagInt(),
+                reader.readZigZagInt(),
+                reader.readZigZagInt()
+            );
+        }
+        write(value: Expiration, writer: yass.Writer): void {
+            writer.writeZigZagInt(value.year);
+            writer.writeZigZagInt(value.month);
+            writer.writeZigZagInt(value.day);
+        }
+        static TYPE_DESC = new yass.TypeDesc(yass.FIRST_ID + 1, new ExpirationHandler);
+    }
+
+    export class Expiration {
+        constructor(public year: number, public month: number, public day: number) {
+            // empty
+        }
+        static TYPE_DESC = ExpirationHandler.TYPE_DESC;
+    }
+
+}
