d1a8fac16f1a5a4071354b7f612018e531354438
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2016-02-23 20:15:22 +0100

    upgraded to typescript 1.8.2

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 set version=0.0.0
 set /p version=Version [ MAJOR.MINOR.PATCH or 'enter' for %version% ]?:
 
-cmd /c npm install -g typescript@1.7.5
+cmd /c npm install -g typescript@1.8.2
 
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_73
 
