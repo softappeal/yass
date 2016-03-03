7dbaf1506ca5757f72a7883c225e18836ab29744
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2016-03-03 20:35:01 +0100

    re-added class util.Reference

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@
 set version=0.0.0
 set /p version=Version [ MAJOR.MINOR.PATCH or 'enter' for %version% ]?:
 
+rem prerequisite: Node.js must be installed
 cmd /c npm install -g typescript@1.8.2
 
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_73
