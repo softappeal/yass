c19dca3681907d9879f32277455d431859069cd4
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2016-03-24 17:01:58 +0100

    typescript and java upgraded

--- build.bat
+++ build.bat
@@ -3,10 +3,10 @@
 set version=0.0.0
 set /p version=Version [ MAJOR.MINOR.PATCH or 'enter' for %version% ]?:
 
-cmd /c npm install -g typescript@1.8.7
+cmd /c npm install -g typescript@1.8.9
 
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_73
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_77
 
-cmd /c C:\development\gradle-2.12\bin\gradle.bat -Pversion=%version%
+cmd /c C:\Users\guru\development\gradle-2.12\bin\gradle.bat -Pversion=%version%
 
 pause
