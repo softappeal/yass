6664d9bbe315bd159f27f3183c8ddef2c5ec2517
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2016-05-24 08:11:41 +0200

    gradle wrapper added

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ set /p version=Version [ MAJOR.MINOR.PATCH or 'enter' for %version% ]?:
 
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_92
 
-cmd /c C:\Users\guru\development\gradle-2.13\bin\gradle.bat -Pversion=%version%
+cmd /c .\gradlew.bat -Pversion=%version%
 
 cd ts
 cmd /c npm install
