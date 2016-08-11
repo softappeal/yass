56941396e4dae329942afd33dac08a089bd71899
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2016-08-11 13:30:40 +0200

    undertow & systemjs bumped

--- py3/test.bat
+++ py3/test.bat
@@ -3,4 +3,5 @@ pip list
 
 python -m unittest test.all_tests
 
-python -m mypy --check-untyped-defs test/contract_test.py test/serialize_test.py test/service_test.py tutorial/socket_client.py tutorial/socket_server.py tutorial/std_server.py
+cmd /c mypy --check-untyped-defs -p tutorial
+cmd /c mypy --check-untyped-defs -p test
