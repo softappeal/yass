6c5fa30e6c57e1f372efff02bc35e42468f63755
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2016-09-30 11:27:26 +0200

    TS 2 migration

--- ts/.coverage.bat
+++ ts/.coverage.bat
@@ -1,2 +1,6 @@
+xcopy yass.*       node_modules\softappeal-yass /I /Y
+xcopy package.json node_modules\softappeal-yass /I /Y
+
 cmd /c node_modules\.bin\istanbul cover test\test-node.js
+
 cmd /c node_modules\.bin\remap-istanbul -i ../build/coverage-js/coverage.json -o ../build/coverage-ts -t html
