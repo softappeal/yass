a481a22c1962187ae29a06d7158c3ed5b57fee26
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2016-10-18 10:47:21 +0200

    cosmetic

--- java/tutorial/ch/softappeal/yass/tutorial/contract/SslConfig.java
+++ java/tutorial/ch/softappeal/yass/tutorial/contract/SslConfig.java
@@ -2,20 +2,16 @@ package ch.softappeal.yass.tutorial.contract;
 
 import ch.softappeal.yass.transport.socket.SslSetup;
 import ch.softappeal.yass.util.FileResource;
-import ch.softappeal.yass.util.Nullable;
 
 import java.security.KeyStore;
 
 public final class SslConfig {
 
-    private static KeyStore readKeyStore(final @Nullable String name) {
-        if (name == null) {
-            return null;
-        }
+    private static KeyStore readKeyStore(final String name) {
         return SslSetup.readKeyStore(new FileResource("certificates/" + name), "StorePass".toCharArray());
     }
 
-    private static SslSetup sslSetup(final @Nullable String keyStore, final @Nullable String trustStore) {
+    private static SslSetup sslSetup(final String keyStore, final String trustStore) {
         return new SslSetup("TLSv1.2", "TLS_RSA_WITH_AES_128_CBC_SHA", readKeyStore(keyStore), "KeyPass".toCharArray(), readKeyStore(trustStore));
     }
 
