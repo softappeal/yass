68ae3f761f7d8a290e808ea72df176ee8d3f1132
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2017-04-29 07:30:29 +0200

    travis added

--- java/yass-base/test/ch/softappeal/yass/util/test/DumperTest.java
+++ java/yass-base/test/ch/softappeal/yass/util/test/DumperTest.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TimeZone;
 
 public class DumperTest {
 
@@ -31,34 +32,40 @@ public class DumperTest {
     }
 
     @Test public void test() {
-        TestUtils.compareFile("ch/softappeal/yass/util/test/DumperTest.dump.txt", new TestUtils.Printer() {
-            void dump(final Dumper dumper, final StringBuilder s, final @Nullable Object value) {
-                s.append(dumper.append(new StringBuilder(256), value)).append('\n');
-            }
-            void print(final Dumper dumper, final PrintWriter printer, final boolean cycles) {
-                final StringBuilder s = new StringBuilder(1024);
-                dump(dumper, s, null);
-                dump(dumper, s, 'c');
-                dump(dumper, s, SerializerTest.createNulls());
-                dump(dumper, s, SerializerTest.createValues());
-                if (cycles) {
-                    dump(dumper, s, SerializerTest.createGraph());
+        final TimeZone timeZone = TimeZone.getDefault();
+        try {
+            TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+            TestUtils.compareFile("ch/softappeal/yass/util/test/DumperTest.dump.txt", new TestUtils.Printer() {
+                void dump(final Dumper dumper, final StringBuilder s, final @Nullable Object value) {
+                    s.append(dumper.append(new StringBuilder(256), value)).append('\n');
                 }
-                dump(dumper, s, Arrays.asList("one", "two", "three").toArray());
-                final Map<Integer, String> int2string = new LinkedHashMap<>();
-                int2string.put(1, "one");
-                int2string.put(2, null);
-                int2string.put(3, "three");
-                dump(dumper, s, int2string);
-                printer.append(s);
-            }
-            @Override public void print(final PrintWriter printer) {
-                print(new TestDumper(false, true, BigInteger.class, BigDecimal.class, Instant.class), printer, true);
-                print(new TestDumper(false, false), printer, false);
-                print(new TestDumper(true, true, BigInteger.class, BigDecimal.class, Instant.class), printer, true);
-                print(new TestDumper(true, false), printer, false);
-            }
-        });
+                void print(final Dumper dumper, final PrintWriter printer, final boolean cycles) {
+                    final StringBuilder s = new StringBuilder(1024);
+                    dump(dumper, s, null);
+                    dump(dumper, s, 'c');
+                    dump(dumper, s, SerializerTest.createNulls());
+                    dump(dumper, s, SerializerTest.createValues());
+                    if (cycles) {
+                        dump(dumper, s, SerializerTest.createGraph());
+                    }
+                    dump(dumper, s, Arrays.asList("one", "two", "three").toArray());
+                    final Map<Integer, String> int2string = new LinkedHashMap<>();
+                    int2string.put(1, "one");
+                    int2string.put(2, null);
+                    int2string.put(3, "three");
+                    dump(dumper, s, int2string);
+                    printer.append(s);
+                }
+                @Override public void print(final PrintWriter printer) {
+                    print(new TestDumper(false, true, BigInteger.class, BigDecimal.class, Instant.class), printer, true);
+                    print(new TestDumper(false, false), printer, false);
+                    print(new TestDumper(true, true, BigInteger.class, BigDecimal.class, Instant.class), printer, true);
+                    print(new TestDumper(true, false), printer, false);
+                }
+            });
+        } finally {
+            TimeZone.setDefault(timeZone);
+        }
     }
 
 }
