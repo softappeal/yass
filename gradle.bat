b6ce3fda51a1e4692dfe27f71088e5b4b29b0310
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2017-10-18 18:39:07 +0200

    python bumped

--- gradle.bat
+++ gradle.bat
@@ -1,3 +1,3 @@
 @echo off
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_144
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_151
 call .\gradlew %*
--- py3/tests.bat
+++ py3/tests.bat
@@ -1,6 +1,6 @@
 call activate py3
 
-pip install mypy==0.521
+pip install mypy==0.530
 
 python -m unittest test.all_tests
 
