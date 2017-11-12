4fd1f65c9437370fe258dd0644b2df0faa90b06a
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2017-11-12 14:38:53 +0100

    made tutorial module more specific

--- java/tutorial/module-info.java
+++ java/tutorial/module-info.java
@@ -1,4 +1,9 @@
-open module ch.softappeal.yass.tutorial { // $todo
+module ch.softappeal.yass.tutorial { // $todo
     requires ch.softappeal.yass.generate;
     requires ch.softappeal.yass.unsupported;
+    opens ch.softappeal.yass.tutorial.contract to ch.softappeal.yass.base;
+    opens ch.softappeal.yass.tutorial.contract.generic to ch.softappeal.yass.base;
+    opens ch.softappeal.yass.tutorial.contract.instrument to ch.softappeal.yass.base;
+    opens ch.softappeal.yass.tutorial.contract.instrument.stock to ch.softappeal.yass.base;
+    opens ch.softappeal.yass.tutorial.contract.instrument.stock.python to ch.softappeal.yass.base;
 }
