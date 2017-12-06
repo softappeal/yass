9a8e43cc5188792f0de45474c34a7bb47c041baf
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2017-12-06 19:13:20 +0100

    gradle bumped

--- java/tutorial/module-info.java
+++ java/tutorial/module-info.java
@@ -1,9 +1,4 @@
-module ch.softappeal.yass.tutorial { // $todo
+open module ch.softappeal.yass.tutorial { // $todo
     requires ch.softappeal.yass.generate;
     requires ch.softappeal.yass.unsupported;
-    opens ch.softappeal.yass.tutorial.contract to ch.softappeal.yass.base;
-    opens ch.softappeal.yass.tutorial.contract.generic to ch.softappeal.yass.base;
-    opens ch.softappeal.yass.tutorial.contract.instrument to ch.softappeal.yass.base;
-    opens ch.softappeal.yass.tutorial.contract.instrument.stock to ch.softappeal.yass.base;
-    opens ch.softappeal.yass.tutorial.contract.instrument.stock.python to ch.softappeal.yass.base;
 }
