45150703eda7736d2721a9313552b51edea8fc34
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2018-09-28 08:38:57 +0200

    gradle and kotlin upgraded

--- gradle.bat
+++ gradle.bat
@@ -1,4 +1,4 @@
 @echo off
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_171
-rem set JAVA_HOME=C:\Program Files\Java\jdk-10.0.1
+rem set JAVA_HOME=C:\Users\guru\development\openjdk-11+28_windows-x64_bin
 call .\gradlew %*
