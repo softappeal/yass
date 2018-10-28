c57dcd218167d412307bd516651b36a8670f89e2
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2018-10-28 21:36:54 +0100

    maven-publish added to build

--- build.bat
+++ build.bat
@@ -1,9 +1,6 @@
 @echo off
 
-set version=0.0.0
-set /p version=Version [ MAJOR.MINOR.PATCH or 'enter' for %version% ]?:
-
-call .\gradle -Pversion=%version%
+call .\gradle
 
 pushd ts
 call .\compile
@@ -16,5 +13,9 @@ popd
 pushd py2
 call .\tests
 popd
+
+set version=0.0.0
+set /p version=Version [ MAJOR.MINOR.PATCH or 'enter' for %version% ]?:
+call .\gradle publish -Pversion=%version%
 
 pause
---
+++ kotlin/tutorial/test/GeneratorTest.kt
@@ -0,0 +1,52 @@
+package ch.softappeal.yass.tutorial
+
+import ch.softappeal.yass.tutorial.generate.GeneratePython2
+import ch.softappeal.yass.tutorial.generate.GeneratePython3
+import ch.softappeal.yass.tutorial.generate.GenerateTypeScript
+import org.junit.Test
+import java.io.File
+import java.util.Arrays
+
+private fun compareDirs(dir1: File, dir2: File) {
+    dir1.listFiles().forEach {
+        val file2 = File(dir2, it.name)
+        if (!file2.exists())
+            throw RuntimeException("$file2 is missing on second")
+        else if (it.isDirectory && !file2.isDirectory)
+            throw RuntimeException("$it: first is a dir, second is a file")
+        else if (!it.isDirectory && file2.isDirectory)
+            throw RuntimeException(it.toString() + "$it: first is a file, second is a dir")
+        else if (it.isDirectory)
+            compareDirs(it, file2)
+        else if (!Arrays.equals(it.readBytes(), file2.readBytes()))
+            throw RuntimeException("$it: content is different")
+    }
+    dir2.listFiles().forEach {
+        val file1 = File(dir1, it.name)
+        if (!file1.exists()) throw RuntimeException("$it is missing on first")
+    }
+}
+
+private fun compareGenerated(module: String) {
+    compareDirs(File("../../$module/tutorial/generated"), File("build/generated/$module"))
+}
+
+class GeneratorTest {
+    @Test
+    fun generateTypeScript() {
+        GenerateTypeScript.main()
+        compareGenerated("ts")
+    }
+
+    @Test
+    fun generatePython3() {
+        GeneratePython3.main()
+        compareGenerated("py3")
+    }
+
+    @Test
+    fun generatePython2() {
+        GeneratePython2.main()
+        compareGenerated("py2")
+    }
+}
