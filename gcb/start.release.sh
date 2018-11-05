434e0d07ac765856362aead330bce8784ef579d8
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2018-11-05 10:45:51 +0100

    gcb: commit and release builds

--- gcb/start.release.sh
+++ gcb/start.release.sh
@@ -3,13 +3,21 @@
 # abort script on error
 set -e
 
+# gcloud kms encrypt --plaintext-file=key.gpg           --ciphertext-file=key.gpg.enc           --location=global --keyring=yass --key=build
+# gcloud kms encrypt --plaintext-file=gradle.properties --ciphertext-file=gradle.properties.enc --location=global --keyring=yass --key=build
+
+# gcloud kms decrypt --plaintext-file=gcb/key.gpg           --ciphertext-file=gcb/key.gpg.enc           --location=global --keyring=yass --key=build
+# gcloud kms decrypt --plaintext-file=gcb/gradle.properties --ciphertext-file=gcb/gradle.properties.enc --location=global --keyring=yass --key=build
+
+# run commit script
 /project/gcb/start.commit.sh
 
 # go to project
 cd /project
 
+# copy secrets
+cp gcb/key.gpg.enc ~/.gradle
+cp gcb/gradle.properties.enc ~/.gradle
+
 # upload to maven central
-pwd
-ls -al
-./gradlew clean
-#./gradlew publish -Pversion=%version%
+./gradlew publish -Pversion=$PROJECT_VERSION
