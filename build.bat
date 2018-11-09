45eb56b9a6f7a0562ac2b5af043541fedb0ceca9
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2018-11-09 17:16:05 +0100

    disabled maven publishing in build.bat

--- build.bat
+++ build.bat
@@ -14,8 +14,8 @@ pushd py2
 call .\tests
 popd
 
-set version=0.0.0
-set /p version=Version [ MAJOR.MINOR.PATCH or 'enter' for %version% ]?:
-call .\gradle publish -Pversion=%version%
+rem set version=0.0.0
+rem set /p version=Version [ MAJOR.MINOR.PATCH or 'enter' for %version% ]?:
+rem call .\gradle publish -Pversion=%version%
 
 pause
