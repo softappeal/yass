48416fc6582e2c02a98633682cbaac1dfc75ce53
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2018-11-09 14:56:58 +0100

    check java 11

--- gcb/start.commit.sh
+++ gcb/start.commit.sh
@@ -5,6 +5,7 @@ set -e
 
 # check java
 java -version
+$JAVA_11_HOME/bin/java -version
 
 # check node
 node --version
@@ -20,8 +21,18 @@ source deactivate
 # go to project
 cd /project
 
-# run gradle
+# prepare gradle
 chmod +x ./gradlew
+
+# check Java 11 compatibility
+# set OLD_JAVA_HOME=$JAVA_HOME
+# export JAVA_HOME=$JAVA_11_HOME
+# ./gradlew --version
+# ./gradlew
+# export JAVA_HOME=$OLD_JAVA_HOME
+
+# run gradle
+./gradlew --version
 ./gradlew
 
 # run ts
