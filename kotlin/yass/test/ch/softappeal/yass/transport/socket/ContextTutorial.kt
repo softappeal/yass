fb5472328f9fc1e1ff1b4580de6b46e8c1241d1b
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2018-11-18 10:04:22 +0100

    gradle.properties.no_secrets added

--- kotlin/yass/test/ch/softappeal/yass/transport/socket/ContextTutorial.kt
+++ kotlin/yass/test/ch/softappeal/yass/transport/socket/ContextTutorial.kt
@@ -27,20 +27,20 @@ internal interface IdentityService {
 
 private val CONTEXT_SERIALIZER = simpleFastSerializer(listOf(IntSerializer, StringSerializer), listOf(Context::class.java))
 private val CONTRACT_SERIALIZER = simpleFastSerializer(listOf(StringSerializer), listOf())
-private val CONTEXT = ContextMessageSerializer(CONTEXT_SERIALIZER, messageSerializer(CONTRACT_SERIALIZER))
-private val IDENTITY_SERVICE = contractId<IdentityService>(1, SimpleMethodMapperFactory)
+private val CONTEXT_MESSAGE_SERIALIZER = ContextMessageSerializer(CONTEXT_SERIALIZER, messageSerializer(CONTRACT_SERIALIZER))
+private val IDENTITY_SERVICE_ID = contractId<IdentityService>(1, SimpleMethodMapperFactory)
 private val ADDRESS = InetSocketAddress("localhost", 28947)
 
 private fun clientSide(client: Client) {
     val contextInterceptor: Interceptor = { _, _, invocation ->
-        CONTEXT.context = Context(123, "Google")
+        CONTEXT_MESSAGE_SERIALIZER.context = Context(123, "Google")
         try {
             invocation()
         } finally {
-            CONTEXT.context = null // cleanup if server returns context
+            CONTEXT_MESSAGE_SERIALIZER.context = null // cleanup if server returns context
         }
     }
-    val identityService = client.proxy(IDENTITY_SERVICE, contextInterceptor)
+    val identityService = client.proxy(IDENTITY_SERVICE_ID, contextInterceptor)
     identityService.createIdentity("Bob")
 }
 
@@ -54,23 +54,23 @@ private class IdentityServiceImpl(private val realmGetter: RealmGetter) : Identi
 
 private fun serverSide(): Service<IdentityService> {
     val contextInterceptor: Interceptor = { _, _, invocation ->
-        println("logging sequenceNumber ${(CONTEXT.context as Context).sequenceNumber}")
+        println("logging sequenceNumber ${(CONTEXT_MESSAGE_SERIALIZER.context as Context).sequenceNumber}")
         try {
             invocation()
         } finally {
-            CONTEXT.context = null // server doesn't return context
+            CONTEXT_MESSAGE_SERIALIZER.context = null // server doesn't return context
         }
     }
-    val realmGetter = { (CONTEXT.context as Context).realm }
-    return service(IDENTITY_SERVICE, IdentityServiceImpl(realmGetter), contextInterceptor)
+    val realmGetter = { (CONTEXT_MESSAGE_SERIALIZER.context as Context).realm }
+    return service(IDENTITY_SERVICE_ID, IdentityServiceImpl(realmGetter), contextInterceptor)
 }
 
 fun main() {
     val executor = Executors.newCachedThreadPool(namedThreadFactory("executor", Terminate))
     try {
-        socketServer(ServerSetup(Server(serverSide()), CONTEXT), executor).start(executor, socketBinder(ADDRESS)).use {
-            clientSide(socketClient(ClientSetup(CONTEXT), socketConnector(ADDRESS)))
-        }
+        socketServer(ServerSetup(Server(serverSide()), CONTEXT_MESSAGE_SERIALIZER), executor)
+            .start(executor, socketBinder(ADDRESS))
+            .use { clientSide(socketClient(ClientSetup(CONTEXT_MESSAGE_SERIALIZER), socketConnector(ADDRESS))) }
     } finally {
         executor.shutdown()
     }
