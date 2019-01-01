121287fc4f2c785384837b8d02423c7d96031f27
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2019-01-01 18:55:35 +0100

    version bumped

--- gradle.bat
+++ gradle.bat
@@ -1,4 +1,4 @@
 @echo off
-    set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_191
-rem set JAVA_HOME=C:\Users\guru\development\openjdk-11.0.1_windows-x64
+rem set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_191
+    set JAVA_HOME=C:\Users\guru\development\openjdk-11.0.1_windows-x64
 call .\gradlew --no-daemon %*
