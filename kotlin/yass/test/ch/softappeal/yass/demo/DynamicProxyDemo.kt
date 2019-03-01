f301f794fb8c721bfbbf78c22645b80bbec9d9b5
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2019-03-01 17:33:12 +0100

    DynamicProxyDemo added to tests

--- kotlin/yass/test/ch/softappeal/yass/demo/DynamicProxyDemo.kt
+++ kotlin/yass/test/ch/softappeal/yass/demo/DynamicProxyDemo.kt
@@ -5,6 +5,8 @@ import java.lang.reflect.*
 import kotlin.coroutines.*
 import kotlin.coroutines.intrinsics.*
 
+// Intercepting suspend functions using a Java dynamic proxy:
+
 interface SFunction {
     suspend fun invoke(): Any?
 }
@@ -13,26 +15,41 @@ val SRemover: Method = SFunction::class.java.methods[0]
 
 typealias SInvoker = suspend (method: Method, args: List<Any?>) -> Any?
 
-fun Method.sInvoke(args: List<Any?>, cont: Continuation<*>, invoker: SInvoker): Any? =
-    SRemover.invoke(object : SFunction {
-        override suspend fun invoke(): Any? = invoker(this@sInvoke, args)
-    }, cont)
+fun Method.sInvoke(
+    args: List<Any?>, continuation: Continuation<*>, invoker: SInvoker
+): Any? = SRemover.invoke(object : SFunction {
+    override suspend fun invoke(): Any? = invoker(this@sInvoke, args)
+}, continuation)
 
 suspend fun Method.sInvoke(implementation: Any, args: List<Any?>): Any? =
-    suspendCoroutineUninterceptedOrReturn { cont ->
-        sInvoke(args, cont) { _, _ -> invoke(implementation, *args.toTypedArray(), cont) }
+    suspendCoroutineUninterceptedOrReturn { continuation ->
+        sInvoke(args, continuation) { _, _ ->
+            invoke(implementation, *args.toTypedArray(), continuation)
+        }
     }
 
-typealias SInvocation = suspend () -> Any?
-typealias SInterceptor = suspend (method: Method, args: List<Any?>, invocation: SInvocation) -> Any?
-
+/** Creates a proxy for [sContract] that intercepts calls to [implementation]. */
 @Suppress("UNCHECKED_CAST")
-fun <C : Any> sProxy(sContract: Class<C>, implementation: C, interceptor: SInterceptor): C =
-    Proxy.newProxyInstance(sContract.classLoader, arrayOf(sContract)) { _, method, args ->
-        method.sInvoke(args.take(args.size - 1), args.last() as Continuation<*>) { _, params ->
-            interceptor(method, params) { method.sInvoke(implementation, params) }
-        }
-    } as C
+fun <C : Any> sProxy(
+    sContract: Class<C>, implementation: C, interceptor: SInterceptor
+): C = Proxy.newProxyInstance(
+    sContract.classLoader, arrayOf(sContract)
+) { _, method, args ->
+    method.sInvoke(
+        args.take(args.size - 1), args.last() as Continuation<*>
+    ) { _, params ->
+        interceptor(method, params) { method.sInvoke(implementation, params) }
+    }
+} as C
+
+// The following code uses the function [sProxy] to intercept suspend functions.
+// It prints:
+//   add[1, 2] called
+//   3
+
+typealias SInvocation = suspend () -> Any?
+typealias SInterceptor =
+    suspend (method: Method, args: List<Any?>, invocation: SInvocation) -> Any?
 
 interface Calculator {
     suspend fun add(a: Int, b: Int): Int
@@ -42,8 +59,8 @@ class CalculatorImpl : Calculator {
     override suspend fun add(a: Int, b: Int) = a + b
 }
 
-val Printer: SInterceptor = { method, _, invocation ->
-    println(method.name)
+val Printer: SInterceptor = { method, args, invocation ->
+    println("${method.name}$args called")
     invocation()
 }
 
@@ -51,3 +68,7 @@ fun main() = runBlocking {
     val calculator = sProxy(Calculator::class.java, CalculatorImpl(), Printer)
     println(calculator.add(1, 2))
 }
+
+// So now the questions are:
+//  - Is this a good/problematic solution?
+//  - Is there a better solution?
