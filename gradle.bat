5f82683224f312dad764bd887f9e196a45d642f9
Angelo Salvade <AngeloSalvade@users.noreply.github.com>
2019-04-02 20:13:13 +0200

    readded --no-daemon

--- gradle.bat
+++ gradle.bat
@@ -1,4 +1,4 @@
 @echo off
 rem set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_191
     set JAVA_HOME=C:\Users\guru\development\openjdk-11.0.2_windows-x64
-call .\gradlew %*
+call .\gradlew --no-daemon %*
